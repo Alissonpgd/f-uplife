59445b52e68eaf24302e356740c7f0b6609a51b3
Alisson Rodrigo da Silva <66378121+Alissonpgd@users.noreply.github.com>
2021-10-13 14:54:02 -0300

    projeto buildado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+require("dotenv").config();
 
 const api = axios.create({
-  baseURL: "https://backend-uplife.herokuapp.com/usuarios",
+  baseURL: 'https://backend-uplife.herokuapp.com',
 });
 
 export default api;
