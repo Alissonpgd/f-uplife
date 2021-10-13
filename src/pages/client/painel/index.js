f319f8fe7d8e4e77e44bab761e10090800448a9e
Alisson Rodrigo da Silva <66378121+Alissonpgd@users.noreply.github.com>
2021-10-13 17:18:19 -0300

    exclusão de imagem

--- src/pages/client/painel/index.js
+++ src/pages/client/painel/index.js
@@ -9,7 +9,7 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import BackGround from '../../../assets/img/UpLife_1.png';
+
 import ARDesevolvimento from '../../../assets/img/ARDesenvolvimento.png';
 //import api from '../../../services/api';
 
@@ -49,10 +49,8 @@ export default function SignInSide() {
       <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
         <Grid item xs={false} sm={4} md={7} >
-          <img src={BackGround} alt="imagem" />
-          <img src={BackGround} alt="imagem" />
-          <img src={BackGround} alt="imagem" />
-          <img src={BackGround} alt="imagem" />
+
+
         </Grid>
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square sx={{ bgcolor: '#ffcc9' }} >
           <Box sx={{ my: 8, mx: 4, display: 'flex', flexDirection: 'column', alignItems: 'center', }} >
